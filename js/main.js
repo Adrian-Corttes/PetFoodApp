a452351b4768d0d144f30be920c451ec2b7aeb7f
Adrian Cortes <ls-adrian@hotmail.com>
2022-03-20 18:27:00 -0500

    We add onscroll to the header

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu__btn').onclick = () =>{
    navbar.classList.toggle('active');
 }
+
+//onscroll to the header
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+
+   if(window.scrollY > 0){
+      document.querySelector('.header').classList.add('active');
+   }
+   else{
+      document.querySelector('.header').classList.remove('active');
+   }
+}
+
+//para que se ejecute la funcion, al terminar de cargar la pagina
+window.onload = () =>{
+   if(window.scrollY > 0){
+      document.querySelector('.header').classList.add('active');
+   }else{
+      document.querySelector('.header').classList.remove('active');
+   }
+}
